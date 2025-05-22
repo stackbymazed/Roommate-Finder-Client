fca750872c9164b26c0472511dfd941a169bcb76
Nayem783982 <majedulislam223311@gmail.com>
2025-05-22 21:10:20 +0600

    implement the React-simple-typewriter

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -4,6 +4,7 @@ import image1 from "../../assets//images (10).jpg"
 import image2 from "../../assets/download (9).jpg"
 import image3 from "../../assets/images (11).jpg"
 import image4 from "../../assets/images (12).jpg"
+import { Typewriter } from 'react-simple-typewriter';
 
 const Banner = () => {
 
@@ -13,7 +14,15 @@ const Banner = () => {
                 {/* Text Section */}
                 <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
                     <h1 className="text-2xl sm:text-3xl lg:text-4xl font-bold leading-none dark:text-black">
-                        Find Your <span className="dark:text-violet-600">Ideal Roommate</span>
+                        Find Your <span className="dark:text-violet-600">
+                            <Typewriter words={['Ideal', 'Best', 'Friendly', 'Good']}
+                                loop={10000}
+                                cursor
+                                cursorStyle='_'
+                                typeSpeed={100}
+                                deleteSpeed={100}
+                                delaySpeed={1000}></Typewriter>
+                             Roommate</span>
                     </h1>
                     <p className="mt-6 mb-8 text-lg sm:mb-12 dark:text-black">
                         Discover compatible roommates based on preferences, lifestyle, and location.Make shared living easy, safe, and affordable—start your roommate journey today.
@@ -26,6 +35,7 @@ const Banner = () => {
                             <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
                             <span className="relative text-black group-hover:text-white"> Find Now</span>
                         </Link>
+
                         <a className="relative inline-block px-4 py-2 font-medium group">
                             <span className="absolute inset-0 w-full h-full transition duration-200 ease-out transform translate-x-1 translate-y-1 bg-black group-hover:-translate-x-0 group-hover:-translate-y-0"></span>
                             <span className="absolute inset-0 w-full h-full bg-white border-2 border-black group-hover:bg-black"></span>
